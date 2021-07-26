8f3df04b6a2446c1f999190ffb52463dc617559d
Shamasis Bhattacharya <shamasis@postman.com>
2021-07-26 16:54:52 +0530

    Added inline code comment to the first release'

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ DebugReporter = function (newman, reporterOptions, options) {
     const silent = Boolean(options.silent || reporterOptions.silent),
         verbose = Boolean(options.verbose || reporterOptions.verbose),
 
+        // create easy and handy utility functions.
+        // these are created here to ensure that they have access to options and instances
+        // from the reporter initialisation.
         log = silent ? function () {} : function (...args) { console.log(...args); },
         deb = (verbose && !silent) ? function (...args) { console.debug(...args); } : function () {};
 
     var clearSummaryStack = function (err, cursor) {
+            // setting array lengths to 0 effectively clears the array.
+            // we prefer this than simply replacing the array variable
+            // with new one.
             newman.summary.run.executions.length = 0;
             newman.summary.run.failures.length = 0;
 
             deb("- [debug] cleared run summary executions and failures.");
         };
 
+    // banner to let know that the debugger is active. silent flag will suppress this.
     log("Debug reporter is active.\n");
 
     if (reporterOptions.forceClearRunSummary) {
@@ -23,6 +30,7 @@ DebugReporter = function (newman, reporterOptions, options) {
         newman.on('item', clearSummaryStack);
     }
 
+    // extra line to separate initialisation output from other reporters.
     log("");
 };
 
