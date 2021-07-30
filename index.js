a5b9673dc12a772ad30276e276fd63ea14902a66
Shamasis Bhattacharya <shamasis@postman.com>
2021-07-30 21:41:58 +0530

    Added initial memwatch support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var DebugReporter;
+
+var MemWatcher = require('node-memwatcher'),
+    DebugReporter;
 
 DebugReporter = function (newman, reporterOptions, options) {
-    const silent = Boolean(options.silent || reporterOptions.silent),
-        verbose = Boolean(options.verbose || reporterOptions.verbose),
+    const silent = Boolean(reporterOptions.silent),
+        verbose = Boolean(reporterOptions.verbose),
 
         // create easy and handy utility functions.
         // these are created here to ensure that they have access to options and instances
@@ -21,10 +23,52 @@ DebugReporter = function (newman, reporterOptions, options) {
         };
 
     // banner to let know that the debugger is active. silent flag will suppress this.
-    log("Debug reporter is active.\n");
+    log("✔ Debug reporter is active.\n");
+
+    if (!silent && options.silent) {
+        log("✔ [warn] Debug reporter does not respect global `--silent` flag.");
+        log("         use --reporter-debug-silent along with --silent\n");
+    }
+
+    // all about memory ======================================================
+    if (reporterOptions.traceMemory) {
+        let graph = Boolean(reporterOptions.traceMemoryGraph),
+            memwatchOptions = {
+                graph: graph,
+                autoHeapDiff: true,
+                gcMetrics: true
+            };
+
+        log("✔ `traceMemory` will attach inspectors to output memory consumption");
+        log("  statistics during the run. To facilitate post process exit stats,");
+        log("  the tracing does not stop until you hit CTRL+C. It is preferred");
+        log("  that you run a long running collection or add significant iterations.");
+
+        if (graph) {
+            log("\n✔ `traceMemoryGraph` is enabled, your other CLI outputs will not be");
+            log("  visible.");
+
+            if (!options.silent) {
+                log("\n✔ [warn] Graph output will be garbage without enabling --silent flag!");
+            }
+
+            memwatchOptions.autoHeapDiff = false;
+        }
+
+        MemWatcher.start(memwatchOptions);
+        MemWatcher.startStatsInterval();
+
+        newman.on('done', function (err) {
+            if (graph) {console.log(Array(24).fill('\n').join(''))} // tmp padding for graph @todo do elegantly later
+            log("✔ Collection execution is complete! `traceMemory` will continue to ");
+            log("  record until you hit CTRL+C");
+        });
+    }
+
+    // all about stack cleaning ======================================================
 
     if (reporterOptions.forceClearRunSummary) {
-        log("! \`forceClearRunSummary\` will remove execution trace after every request.");
+        log("✔ \`forceClearRunSummary\` will remove execution trace after every request.");
         log("  Note that this may have adverse effects on other reporters if they depend");
         log("  on this trace data.");
         newman.on('item', clearSummaryStack);
@@ -32,6 +76,10 @@ DebugReporter = function (newman, reporterOptions, options) {
 
     // extra line to separate initialisation output from other reporters.
     log("");
+
+    newman.on('done', function () {
+        log("\n✔ Debug reporter inactive.\n")
+    })
 };
 
 module.exports = DebugReporter;
